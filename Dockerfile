45a7afe855ad9254694b7c889d6d6a4c7ab4bf43
João Azevedo <joao.azevedo@ufv.br>
2022-04-29 09:03:38 -0300

    config docker and lint

--- Dockerfile
+++ Dockerfile
@@ -1,31 +1,27 @@
-# ----------------------DEVELOPMENT------------------------
-FROM node16.14.2 AS development
+FROM node:16.14.2-slim
+
+RUN apt-get update && apt-get install dumb-init libssl1.1
+
+ENV NODE_ENV=production
+ENV DATABASE_URL=postgres://giavsqatswmdrk:63af5a25592db45074fda281824560ab5ff047790b6fb1f1796049eb8e556671@ec2-3-224-125-117.compute-1.amazonaws.com:5432/ddivnq1nu7lnk3?schema=public
 
 # Making sure application directory is owned by 'node' user
 RUN mkdir /srv/valoriza && chown node:node /srv/valoriza
-
-# This avoid to use 'root' user (good practice)
-USER node
 
 WORKDIR /srv/valoriza
 
 # Copy only dependencies installation files, so Docker can cache 'node_modules'
 COPY --chown=node:node package.json package-lock.json ./
 
-RUN npm install
-
-
-# ----------------------PRODUCTION-------------------------
-FROM node16.14.2-slim AS production
-
-USER node
-
-WORKDIR /srv/valoriza
-
-# Making sure 'node' user can only read 'node_modules' at production
-# This COPY is taking advantage of the 'node_modules' build previously at development
-COPY --from=development --chown=root:root /srv/valoriza/node_modules ./node_modules
+# Install dependencies
+RUN npm ci --only=production
 
 COPY . .
+
+RUN npx prisma migrate deploy && npx prisma generate
+
+RUN ls /srv/valoriza
+
+USER node
 
 CMD ["npm", "run", "start"]
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
+/* eslint-disable no-console */
 import express, { Application, NextFunction, Request, Response } from 'express';
-import "express-async-errors";
+import 'express-async-errors';
+import morgan from 'morgan';
 import { router } from './routes';
 
 const app: Application = express();
+
+app.use(morgan('dev'));
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -10,23 +14,35 @@ app.use(express.json());
 
 app.use(router);
 
-app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
-  if (err instanceof Error) {
-    return response.status(400).json({
-      error: err.message,
+app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        error: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error',
     });
   }
+);
 
-  return response.status(500).json({
-    status: "error",
-    message: "Internal Server Error",
-  })
-});
+const port = 3333;
 
-const port: number = 3333;
-
-app.get("/", (_req, res: Response) => {
+app.get('/', (_req, res: Response) => {
   res.send(`Server is running on port: ${port}`);
 });
 
-app.listen(port, () => console.log(`🚀 Server is running on port ${port}.`));
+const server = app.listen(port, () =>
+  console.log(`🚀 Server is running on port ${port}.`)
+);
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM signal received: closing HTTP server');
+  server.close(() => {
+    console.log('HTTP server closed');
+  });
+});
